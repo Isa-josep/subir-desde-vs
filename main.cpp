e4f04266f720efc754b512c3af4b67d0ea5c7d4e
Isa-josep <isaurini1902@gmail.com>
2024-03-24 23:36:03 -0600

    main

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#ifdef  LOCAL 
+#include <debug.h>  
+#else 
+#define  debug(...)
+#endif
+using namespace std;
+#define endl '\n'
+#define ll long long 
+#define ull  unsigned long long 
+#define pb push_back
+#define fore(i,a,z) for(int i=a;i<z;i++)
+#define len(n) n.size()
+#define all(n) n.begin(),n.end()
+int main(){
+cin.tie(0)->sync_with_stdio(0),cout.tie(0);
+    
+    return 0;
+}
